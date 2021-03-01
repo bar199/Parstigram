44241a4cd70b12ab0673d11563fe4a5a7e4f0ed3
bar199 <35604629+bar199@users.noreply.github.com>
2021-02-28 22:00:15 -0600

    Initial commit

---
+++ app/src/main/java/com/example/parstigram/ParseApplication.java
@@ -0,0 +1,24 @@
+package com.example.parstigram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+    // Initializes Parse SDK as soon as the application is created
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("FVYcMzM5bmJGC9Rsb7nMXCGKl6eDRfEBrjSqwy4h")
+                .clientKey("NWbGZvG0sMmhpZuFKRKrrsZqDNNxKNVcOZ9bbe0D")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}

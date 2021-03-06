73e2800e5e53a84c2278e1c3db9e94d095a407bf
bar199 <35604629+bar199@users.noreply.github.com>
2021-03-05 19:24:29 -0600

    Episode 7 - Bottom Navigation View

--- app/src/main/java/com/example/parstigram/MainActivity.java
+++ app/src/main/java/com/example/parstigram/MainActivity.java
@@ -6,17 +6,23 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.FileProvider;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
@@ -34,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnLogout;
     private ProgressBar pbProgress;
     private File photoFile;
+    private BottomNavigationView bottomNavigationView;
     public String photoFileName = "photo.jpg";
 
     @Override
@@ -47,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
         btnSubmit = findViewById(R.id.btnSubmit);
         btnLogout = findViewById(R.id.btnLogout);
         pbProgress = findViewById(R.id.pbProgress);
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
 
         btnCaptureImage.setOnClickListener(v -> {
             launchCamera();
@@ -78,6 +86,26 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "Logged out!", Toast.LENGTH_SHORT).show();
             } else {
                 Toast.makeText(this, "Error logging out!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_home:
+                        Toast.makeText(MainActivity.this, "Home!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_compose:
+                        Toast.makeText(MainActivity.this, "Compose!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_profile:
+                        Toast.makeText(MainActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
+                    default:
+                        break;
+                }
+                return true;
             }
         });
     }
